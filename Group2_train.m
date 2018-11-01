7d293c767b3168b6b66da2bf4a5188b6444236b9
JayB9887 <39125593+JayB9887@users.noreply.github.com>
2018-11-01 10:53:10 -0400

    Add files via upload

---
+++ Group2_train.m
@@ -0,0 +1,12 @@
+%% Group: 2         Time: Tues-Thurs
+%  Names: Jay, James, Michael, Noah
+
+
+clear    all;
+close    all;
+delete(instrfindall);
+clc;
+
+%% Connect equipment
+a = arduino('COM4');
+
